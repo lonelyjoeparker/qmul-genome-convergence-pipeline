e3759e80334119dec6504a546ea47e168b798564
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2011-09-14 00:04:47 +0000

    Wireframe method stubs for core classes

---
+++ src/uk/ac/qmul/sbcs/evolution/convergence/handlers/PamlAnalysis.java
@@ -0,0 +1,14 @@
+package uk.ac.qmul.sbcs.evolution.convergence.handlers;
+
+public abstract class PamlAnalysis {
+	private float[] sitewiseLikelihoods;
+	private int numberOfDatasets;
+	private int numberOfSitePatterns;
+	
+	public PamlAnalysis(){}
+	
+	public void Initialize(){}
+	
+	public void RunAnalysis(){}
+	
+}

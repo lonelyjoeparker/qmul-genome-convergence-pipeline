9e678c42dbb8ec5a1e3cc65139de04caa3ce628d
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2011-09-19 17:55:11 +0000

    Refactored all packages to trunk

---
+++ trunk/uk/ac/qmul/sbcs/evolution/convergence/handlers/AamlAnalysis.java
@@ -0,0 +1,5 @@
+package uk.ac.qmul.sbcs.evolution.convergence.handlers;
+
+public class AamlAnalysis extends PamlAnalysis {
+
+}
---
+++ trunk/uk/ac/qmul/sbcs/evolution/convergence/handlers/BasemlAnalysis.java
@@ -0,0 +1,5 @@
+package uk.ac.qmul.sbcs.evolution.convergence.handlers;
+
+public class BasemlAnalysis extends PamlAnalysis {
+
+}

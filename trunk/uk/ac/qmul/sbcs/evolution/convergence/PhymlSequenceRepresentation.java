ab5d8c4d8755a14a9c6a5d851b9b8df2992e79d5
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2011-09-19 18:52:02 +0000

    Implement Perl wireframe code.

--- trunk/uk/ac/qmul/sbcs/evolution/convergence/PhymlSequenceRepresentation.java
+++ trunk/uk/ac/qmul/sbcs/evolution/convergence/PhymlSequenceRepresentation.java
@@ -3,16 +3,22 @@ package uk.ac.qmul.sbcs.evolution.convergence;
 import java.io.*;
 import java.util.*;
 
+/*
+ * @author Joe Parker, Kitson Consulting Ltd/Queen Mary, University of London: http://code.google.com/a/eclipselabs.org/u/joeparkerandthemegahairymen/
+ * @version 0.1
+ */
+		
 public class PhymlSequenceRepresentation {
 	private File file;
 	private String inputFileDatatype;
 	private String inputSequenceDatatype;
 	private ArrayList<String> rawInput;
-	private String sequenceType;
+	private SequenceFileFormat sequenceFormat;
 	private int numberOfSites;
 	private int numberOfTaxa;
 	private HashSet<char[]> sequenceHash;
 	private HashSet<String> truncatedNamesHash;
+	private boolean sequenceFileTypeSet = false;
 	
 	public void PhymlSequenceRespresentation(){}
 	
@@ -26,7 +32,15 @@ public class PhymlSequenceRepresentation {
 		}
 	}
 	
-	public void determineInputFileDatatype(){}
+	public SequenceFileFormat determineInputFileDatatype(){
+		if(sequenceFileTypeSet){
+			return this.sequenceFormat;
+		}else{
+			this.sequenceFormat = SequenceFileFormat.NATIVE;
+			//TODO: implement Perl wireframe code.
+			return this.sequenceFormat;
+		}
+	}
 	
 	public void determineInputSequenceType(){}
 	

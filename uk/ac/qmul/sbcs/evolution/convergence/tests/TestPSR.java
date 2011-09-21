537b7cbcf2145dfa348d4837f6b7a619cb73cd0e
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2011-09-21 16:30:30 +0000

    PhymlSequenceRepresentation test harness

--- uk/ac/qmul/sbcs/evolution/convergence/tests/TestPSR.java
+++ uk/ac/qmul/sbcs/evolution/convergence/tests/TestPSR.java
@@ -3,21 +3,36 @@ package uk.ac.qmul.sbcs.evolution.convergence.tests;
 import java.io.*;
 import java.util.*;
 import uk.ac.qmul.sbcs.evolution.convergence.*;
+import javax.swing.*;
 
 public class TestPSR {
 
+	private JFileChooser chooser = new JFileChooser();
+	private File inputFile;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("trying to read "+args[0]+" file\n");
-		File inputFile = new File(args[0]);
-		PhymlSequenceRepresentation PSR = new PhymlSequenceRepresentation();
-		PSR.loadSequences(inputFile);
-		ArrayList<String> rawFileContents = PSR.getRawInput();
-		System.out.println(rawFileContents.get(0));
-		System.out.println(rawFileContents.get(rawFileContents.size()-1));
+		new TestPSR().go();
 	}
-
+	
+	public void go(){
+		int fileChosenStatus = chooser.showOpenDialog(null);
+		if(fileChosenStatus == JFileChooser.APPROVE_OPTION){
+			try {
+				inputFile = chooser.getSelectedFile();
+				System.out.println("trying to read "+inputFile.getAbsolutePath()+" file\n");
+				PhymlSequenceRepresentation PSR = new PhymlSequenceRepresentation();
+				PSR.loadSequences(inputFile);
+				ArrayList<String> rawFileContents = PSR.getRawInput();
+				System.out.println(rawFileContents.get(0));
+				System.out.println(rawFileContents.get(rawFileContents.size()-1));
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }

dbb3b3a45f9e53ffc060a3bd62f0250e370a820b
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2011-10-18 22:50:59 +0000

    Stub test classes for ASR Simulate Convergence [DNA, codons] methods

---
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/tests/TestPSRsimulateConvergenceDNA.java
@@ -0,0 +1,55 @@
+package uk.ac.qmul.sbcs.evolution.convergence.tests;
+
+import java.io.*;
+import java.util.*;
+import uk.ac.qmul.sbcs.evolution.convergence.*;
+import uk.ac.qmul.sbcs.evolution.convergence.util.*;
+import javax.swing.*;
+
+public class TestPSRsimulateConvergenceDNA {
+
+	private JFileChooser chooser = new JFileChooser();
+	private File inputFile;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TestPSRsimulateConvergenceDNA().go();
+	}
+	
+	public void go(){
+		int fileChosenStatus = chooser.showOpenDialog(null);
+		if(fileChosenStatus == JFileChooser.APPROVE_OPTION){
+			try {
+				inputFile = chooser.getSelectedFile();
+				System.out.println("trying to read "+inputFile.getAbsolutePath()+" file\n");
+				AlignedSequenceRepresentation PSR = new AlignedSequenceRepresentation();
+				try{
+					PSR.loadSequences(inputFile);
+				}catch(TaxaLimitException ex){
+					ex.printStackTrace();
+				}
+/*
+				ArrayList<String> rawFileContents = PSR.getRawInput();
+				System.out.println("\n\nfirst:\t"+rawFileContents.get(0));
+				System.out.println("last:\t"+rawFileContents.get(rawFileContents.size()-1));
+*/
+				PSR.printShortSequences(20);
+				System.out.println("read "+PSR.getNumberOfSites()+" sites and "+PSR.getNumberOfTaxa()+" taxa.");
+				
+				String first = PSR.getTaxaList().first();
+				String last = PSR.getTaxaList().last();
+				
+				System.out.println("converging "+last+" sequence to "+first);
+				String[] taxaToForce = {last};
+				PSR.simulateConvergence(first, taxaToForce, 69);
+				PSR.printShortSequences(20);
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}

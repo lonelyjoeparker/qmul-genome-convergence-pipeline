39ffdd282e43e068a85c14de1e50497e82232bf8
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2011-12-05 12:00:39 +0000

    Simulated convergence test to explore power - initial import.

---
+++ trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/runners/CongruenceTestPowerSim.java
@@ -0,0 +1,12 @@
+package uk.ac.qmul.sbcs.evolution.convergence.runners;
+
+/**
+ * 
+ * @author Joe Parker
+ * @since 09/11/2011
+ *
+ * Runner main class for simulations to determine power of congruence test method.
+ */
+public class CongruenceTestPowerSim {
+	
+}

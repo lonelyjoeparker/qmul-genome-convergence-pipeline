50605a6028c199f4f55825b91ca5064848207c38
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2012-01-06 17:50:21 +0000

    UnitPowerSimulationHardcoded and various dependencies.

--- trunk/src/uk/ac/qmul/sbcs/evolution/convergence/analyses/UnitPowerSimulation.java
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/analyses/UnitPowerSimulation.java
@@ -15,6 +15,7 @@ import uk.ac.qmul.sbcs.evolution.convergence.handlers.EvolverSimulationSGE;
 import uk.ac.qmul.sbcs.evolution.convergence.handlers.documents.PamlDocument.AamlParameters;
 import uk.ac.qmul.sbcs.evolution.convergence.util.TaxaLimitException;
 import uk.ac.qmul.sbcs.evolution.convergence.util.stats.DataSeries;
+import uk.ac.qmul.sbcs.evolution.convergence.util.stats.PercentileOutOfRangeError;
 
 public class UnitPowerSimulation {
 
@@ -71,6 +72,7 @@ public class UnitPowerSimulation {
 	 * @param taxaList
 	 */
 	public UnitPowerSimulation(EvolverSimulationSGE dataSimulation, EvolverSimulationSGE nullSimulation, File treeFile, File workDir, File binariesLocation, String runID, String model, int datasetSize, int nullDataSize, int sitesToConverge, String[] taxaToConverge, String[] taxaToRemove, String masterTaxon, SequenceCodingType sequenceCodingType,TreeSet<String> taxaList){
+		this.numberOfTaxa = taxaList.size();
 		this.dataSimulation = dataSimulation;
 		this.nullSimulation = nullSimulation;
 		this.treeFile = treeFile;
@@ -84,7 +86,8 @@ public class UnitPowerSimulation {
 		this.taxaToConverge = taxaToConverge;
 		this.taxaToRemove = taxaToRemove;
 		this.masterTaxon = masterTaxon;
-		this.sequenceCodingType = sequenceCodingType;this.taxaList = taxaList;
+		this.sequenceCodingType = sequenceCodingType;
+		this.taxaList = taxaList;
 	}
 	
 	public String simulatePower(){
@@ -131,6 +134,7 @@ public class UnitPowerSimulation {
 		AaMLparameters.put(AamlParameters.TREEFILE, "treefile = "+this.treeFile.getAbsolutePath());
 		AaMLparameters.put(AamlParameters.OUTFILE, "outfile = "+aaDataAnalysisOutputFile.getAbsolutePath());
 		AaMLparameters.put(AamlParameters.AARATEFILE, "aaRatefile = "+this.binariesLocation.getAbsolutePath()+"/dat/wag.dat");
+		AaMLparameters.put(AamlParameters.ALPHA, "alpha = 0.2545");
 		File[] treefiles = {this.treeFile};
 		AlignedSequenceRepresentation[] datasets = {simulatedData};
 		AamlAnalysisSGE dataAaml = new AamlAnalysisSGE(datasets, treefiles, AaMLparameters,"aamlOnData.ctl");
@@ -152,7 +156,7 @@ public class UnitPowerSimulation {
 		
 		// simulation only
 		
-		nullSimulation.pokeSeed();
+//		nullSimulation.pokeSeed();
 		nullSimulation.simulateNoArg();
 		simulatedNull = new AlignedSequenceRepresentation();
 		try {
@@ -172,6 +176,7 @@ public class UnitPowerSimulation {
 		AaMLparameters.put(AamlParameters.OUTFILE, "outfile = "+aaNullAnalysisOutputFile.getAbsolutePath());
 		AaMLparameters.put(AamlParameters.TREEFILE, "treefile = "+this.treeFile.getAbsolutePath());
 		AaMLparameters.put(AamlParameters.AARATEFILE, "aaRatefile = "+this.binariesLocation.getAbsolutePath()+"/dat/wag.dat");
+		AaMLparameters.put(AamlParameters.ALPHA, "alpha = 0.2545");
 		datasets[0] = simulatedNull;
 
 		AamlAnalysisSGE nullAaml = new AamlAnalysisSGE(datasets, treefiles, AaMLparameters,"aamlOnNull.ctl");
@@ -190,6 +195,80 @@ public class UnitPowerSimulation {
 		}
 		nulllnL = simulatedNull.getFullSitesLnL(nullSSLS);
 		
-		return "return string value.\ndata mean\t"+datalnL.getMean()+"\nnull mean\t"+nulllnL.getMean();
+		float dataMean = datalnL.getMean();
+		float dataMedi = datalnL.getMedian();
+		float data95ci;
+		float data05ci;
+		float nullMean = nulllnL.getMean();
+		float nullMedi = nulllnL.getMedian();
+		float null95ci;
+		float null10ci;
+		float null09ci;
+		float null08ci;
+		float null07ci;
+		float null06ci;
+		float null05ci;
+		float null04ci;
+		float null03ci;
+		float null02ci;
+		float null01ci;
+		try {
+			data95ci = datalnL.getValueAtPercentile(95);
+			data05ci = datalnL.getValueAtPercentile(5);
+			null95ci = nulllnL.getValueAtPercentile(95);
+			null10ci = nulllnL.getValueAtPercentile(10);
+			null09ci = nulllnL.getValueAtPercentile(9);
+			null08ci = nulllnL.getValueAtPercentile(8);
+			null07ci = nulllnL.getValueAtPercentile(7);
+			null06ci = nulllnL.getValueAtPercentile(6);
+			null05ci = nulllnL.getValueAtPercentile(5);
+			null04ci = nulllnL.getValueAtPercentile(4);
+			null03ci = nulllnL.getValueAtPercentile(3);
+			null02ci = nulllnL.getValueAtPercentile(2);
+			null01ci = nulllnL.getValueAtPercentile(1);
+		} catch (PercentileOutOfRangeError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			data95ci = 999.9f;
+			data05ci = 999.9f;
+			null95ci = 999.9f;
+			null10ci = 999.9f;
+			null09ci = 999.9f;
+			null08ci = 999.9f;
+			null07ci = 999.9f;
+			null06ci = 999.9f;
+			null05ci = 999.9f;
+			null04ci = 999.9f;
+			null03ci = 999.9f;
+			null02ci = 999.9f;
+			null01ci = 999.9f;
+		}
+		int dataObsLessThan10 = datalnL.numObservationsLessThan(null10ci);
+		int dataObsLessThan9 = datalnL.numObservationsLessThan(null09ci);
+		int dataObsLessThan8 = datalnL.numObservationsLessThan(null08ci);
+		int dataObsLessThan7 = datalnL.numObservationsLessThan(null07ci);
+		int dataObsLessThan6 = datalnL.numObservationsLessThan(null06ci);
+		int dataObsLessThan5 = datalnL.numObservationsLessThan(null05ci);
+		int dataObsLessThan4 = datalnL.numObservationsLessThan(null04ci);
+		int dataObsLessThan3 = datalnL.numObservationsLessThan(null03ci);
+		int dataObsLessThan2 = datalnL.numObservationsLessThan(null02ci);
+		int dataObsLessThan1 = datalnL.numObservationsLessThan(null01ci);
+		
+		StringBuilder output = new StringBuilder();
+		
+		output.append(this.numberOfTaxa+"\t"+this.datasetSize+"\t"+this.nullDataSize+"\t"+this.sitesToConverge+"\t"+this.taxaToConverge.length+"\t");
+		output.append("Dataset\t95%\tMean\tMedian\t5%\tData\t"+data95ci+"\t"+dataMean+"\t"+dataMedi+"\t"+data05ci+"\tNull\t"+null95ci+"\t"+nullMean+"\t"+nullMedi+"\t"+null05ci+"\t");
+		output.append("Num sites < % (Actual convergent sites = "+this.sitesToConverge+")\t");
+		output.append("10\t"+dataObsLessThan10+"\t");
+		output.append("09\t"+dataObsLessThan9+"\t");
+		output.append("08\t"+dataObsLessThan8+"\t");
+		output.append("07\t"+dataObsLessThan7+"\t");
+		output.append("06\t"+dataObsLessThan6+"\t");
+		output.append("05\t"+dataObsLessThan5+"\t");
+		output.append("04\t"+dataObsLessThan4+"\t");
+		output.append("03\t"+dataObsLessThan3+"\t");
+		output.append("02\t"+dataObsLessThan2+"\t");
+		output.append("01\t"+dataObsLessThan1+"\t");
+		return output.toString();
 	}
 }
--- trunk/src/uk/ac/qmul/sbcs/evolution/convergence/runners/UnitPowerSimulationRunnerHardcoded.java
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/runners/UnitPowerSimulationRunnerHardcoded.java
@@ -9,6 +9,7 @@ import java.util.TreeSet;
 import uk.ac.qmul.sbcs.evolution.convergence.SequenceCodingType;
 import uk.ac.qmul.sbcs.evolution.convergence.analyses.UnitPowerSimulation;
 import uk.ac.qmul.sbcs.evolution.convergence.handlers.EvolverSimulationSGE;
+import uk.ac.qmul.sbcs.evolution.convergence.util.CustomFileWriter;
 
 /**
  * 
@@ -16,6 +17,16 @@ import uk.ac.qmul.sbcs.evolution.convergence.handlers.EvolverSimulationSGE;
  * @since 2012/01/04
  * 
  * Handler class for the UnitSimulation - hardcoded.
+ * 
+ * <p><Arguments>
+ * <p>java -jar <b>UnitPowerSimulationHardcoded.jar</b> <i>workDir</i> <i>binariesDir</i> <i>dataSetSize</i> <i>nullSetSize</i> <i>numberOfConvergentSitesToSimulate</i>
+ * <p><ol>
+ * <li><i>workDir</i> - Fully qualified path to work directory.</li>
+ * <li><i>binariesDir</i> - Fully qualified path to directory where PAML binaries are located.</li>
+ * <li><i>dataSetSize</i> - Number of sites in simulated 'data' alignment.</li>
+ * <li><i>nullSetSize</i> - Number of sites in simulated 'null' alignment.</li>
+ * <li><i>numberOfConvergentSitesToSimulate</i> - Number of sites that should be forced to converge.</li>
+ * </ol>
  */
 public class UnitPowerSimulationRunnerHardcoded {
 	
@@ -28,6 +39,7 @@ public class UnitPowerSimulationRunnerHardcoded {
 	private int nullDataSize;
 	private int numberOfTaxa;
 	private int numberOfSitesToConverge;
+	private String[] inputArgs;
 	private String[] taxaToRemove;
 	private String[] taxaToConverge;
 	private String masterTaxon;
@@ -38,15 +50,16 @@ public class UnitPowerSimulationRunnerHardcoded {
 	private HashMap<String,String> simulationParameters;
 	private TreeSet<String> taxaList;
 	
+	public UnitPowerSimulationRunnerHardcoded(String[] args){
+		this.inputArgs = args;
+	}
+	
 	public static void main(String[] args){
-		UnitPowerSimulationRunnerHardcoded aUnit = new UnitPowerSimulationRunnerHardcoded();
+		UnitPowerSimulationRunnerHardcoded aUnit = new UnitPowerSimulationRunnerHardcoded(args);
 		aUnit.go();
 	}
 	
 	public void go(){
-		this.initialiseTaxaList();
-		this.initialseSimulationParameters();
-		
 		
 		/*
 		 * Execution:
@@ -58,22 +71,24 @@ public class UnitPowerSimulationRunnerHardcoded {
 		 */
 		
 		treeFile = new File("/pamlTest/trialPowerSim/22t_WAG.tre");
-		workDir = new File("/pamlTest/trialPowerSim/");
-		binariesLocation = new File("/pamlTest/binaries/");
-		evolverBinary = new File("/pamlTest/binaries/evolver");
-		evolverCtl = new File("/pamlTest/trialPowerSim/evolver.ctl");
+		workDir = new File(inputArgs[0]);
+		binariesLocation = new File(inputArgs[1]);
+		evolverBinary = new File(binariesLocation.getAbsolutePath()+"/evolver");
+		evolverCtl = new File(workDir.getAbsolutePath()+"/evolver.ctl");
 		runID = "UnitPowerSimHC";
 		model = "WAG";
 		tree = "(Monodelphi:0.24760393,(((((((Bos:0.048904167,Tursiops:0.031684545):0.0090338518,Vicugna:0.042972451):0.012554305,Equus:0.039082616):0.0018692541,((Canis:0.035796925,Felis:0.031967411):0.018314869,(((Eidolon:0.019770896,Pteropus:0.012449182):0.028960857,(Megaderma:0.055404706,Rhinolophu:0.039700734):0.006976852):0.0032567174,(Myotis:0.05253924,Pteronotus:0.053246857):0.011490039):0.008641665):0.00091422006):0.0025944456,(Erinaceus:0.089282695,Sorex:0.10153418):0.01661825):0.0063002763,((Homo:0.0028293894,Pan:0.0028236869):0.04126969,(Mus:0.12335404,(Ochotona:0.072903436,Oryctolagu:0.042011259):0.029730032):0.0093948645):0.0074159284):0.0092128208,Loxodonta:0.062597437):0.0046032988,Dasypus:0.066898101);";
-		datasetSize = 200;
-		nullDataSize = 500;
-		numberOfSitesToConverge = 2;
+		datasetSize = new Integer(inputArgs[2]);
+		nullDataSize = new Integer(inputArgs[3]);
+		numberOfSitesToConverge = new Integer(inputArgs[4]);
 		taxaToConverge = new String[2];
-		taxaToConverge[0] = "Tursiops";
-		taxaToConverge[1] = "Pteronotus";
+		taxaToConverge[0] = "TURSIOPS";
+		taxaToConverge[1] = "PTERONOTUS";
 		taxaToRemove = new String[0];
 		masterTaxon = "RHINOLOPHU";
 		sequenceCodingType = SequenceCodingType.AA;
+		this.initialiseTaxaList();
+		this.initialseSimulationParameters();
 		
 		EvolverSimulationSGE dataSimulation = new EvolverSimulationSGE(evolverBinary, workDir, evolverCtl, tree, 22, datasetSize, 1, sequenceCodingType);
 		EvolverSimulationSGE nullSimulation = new EvolverSimulationSGE(evolverBinary, workDir, evolverCtl, tree, 22, nullDataSize, 1, sequenceCodingType);
@@ -92,14 +107,20 @@ public class UnitPowerSimulationRunnerHardcoded {
 		UnitPowerSimulation simulation = new UnitPowerSimulation(dataSimulation, nullSimulation, treeFile, workDir, binariesLocation, runID, model, datasetSize, nullDataSize, numberOfSitesToConverge, taxaToConverge, taxaToRemove, masterTaxon, sequenceCodingType, taxaList);
 		
 		String retVal = simulation.simulatePower();
+		File finalOutput = new File(workDir.getAbsolutePath()+"/sim.out.txt");
 		System.out.println(retVal);
+		CustomFileWriter outputWriter = new CustomFileWriter(finalOutput,retVal);
 	}
 
 	private void initialseSimulationParameters() {
 		simulationParameters = new HashMap<String,String>();
-		simulationParameters.put("AARATEFILE", "/pamlTest/binaries/dat/wag.dat");
+		simulationParameters.put("AARATEFILE", binariesLocation.getAbsolutePath()+"/dat/wag.dat");
 		simulationParameters.put("TREE_LENGTH", "-1");
 		simulationParameters.put("PAMLFLAG", "0");
+		simulationParameters.put("ALPHA", "0.2545");
+		simulationParameters.put("AAFREQS", 
+				"0.1055  0.0202  0.0592  0.0474  0.0061  0.0309  0.0390  0.0763  0.0251  0.0754 \n" +
+				"0.1046  0.0468  0.0121  0.0410  0.0523  0.0668  0.0355  0.0009  0.0526  0.1020 ");
 		//TODO Add the other simulation parameters (hardcoded for now)
 	}
 

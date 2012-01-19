3c73429623fc436e0d155ed6e7bd1e5e5c8b06cf
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2012-01-19 00:09:17 +0000

    Custom Exception for filterForMissingData() method in AlignedSequenceRepresentation class.

---
+++ trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/FilterOutOfAllowableRangeException.java
@@ -0,0 +1,15 @@
+package uk.ac.qmul.sbcs.evolution.convergence;
+
+/**
+ * 
+ * @author joeparker
+ * @since 0.0.1 r1
+ * @see AlignedSequenceRepresentation
+ * This class is thrown whenever the filterForMissingData() method in AlignedSequenceRepresentation is called with inappropriate filter. 
+ * The allowable filter bounds are:
+ * 	0<filter²100 	- when using filterFactor = true;
+ *  0<numberOfTaxa 	- when using filterFactor = false (e.g., threshold no. of gaps per site.
+ */
+public class FilterOutOfAllowableRangeException extends Exception {
+
+}

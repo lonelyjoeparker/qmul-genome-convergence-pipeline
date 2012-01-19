93677b47c5537539bc4a818a2a76ed39b40b9776
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2012-01-19 02:32:38 +0000

    A runner class for an AA congruence test analysis with 2 initial trees + 1 RAxML tree, estimated model parameters, simulated null distributions and data filtered for gaps.

---
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/runners/CongruenceTestRunnerAAwithBinariesArgsPruningSimFilters.java
@@ -0,0 +1,57 @@
+package uk.ac.qmul.sbcs.evolution.convergence.runners;
+
+import java.io.File;
+import java.util.TreeSet;
+
+import uk.ac.qmul.sbcs.evolution.convergence.analyses.*;
+
+public class CongruenceTestRunnerAAwithBinariesArgsPruningSimFilters {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		assert(args.length==7);
+		File dataSet = new File(args[0]);
+		File treeFileOne = new File(args[1]);
+		File treeFileTwo = new File(args[2]);
+		File workDir = new File(args[3]);
+		String runID = args[4];
+		File binaries = new File(args[5]);
+		Integer replicatesForNull = Integer.parseInt(args[6]);
+		int thisFilter = Integer.parseInt(args[7]);
+		boolean doFactor = false;
+		switch(Integer.parseInt(args[8])){
+			case (1): doFactor = true; break;
+			case (0): doFactor = false; break;
+		}
+		TreeSet<String> taxaList = new TreeSet<String>();
+		taxaList.add("TURSIOPS");
+		taxaList.add("CANIS");
+		taxaList.add("FELIS");
+		taxaList.add("LOXODONTA");
+		taxaList.add("ERINACEUS");
+		taxaList.add("MUS");
+		taxaList.add("MONODELPHIS");
+		taxaList.add("PAN");
+		taxaList.add("HOMO");
+		taxaList.add("PTERONOTUS");
+		taxaList.add("RHINOLOPHUS");
+		taxaList.add("PTEROPUS");
+		taxaList.add("EIDOLON");
+		taxaList.add("DASYPUS");
+		taxaList.add("EQUUS");
+		taxaList.add("MEGADERMA");
+		taxaList.add("MYOTIS");
+		taxaList.add("BOS");
+		/* The extra 4 taxa in the 22 taxon tree */
+		taxaList.add("VICUGNA");
+		taxaList.add("OCHOTONA");
+		taxaList.add("ORYCTOLAGUS");
+		taxaList.add("SOREX");
+		CongruenceAnalysisAAWithBinariesPruningSimulationFiltering analysis = new CongruenceAnalysisAAWithBinariesPruningSimulationFiltering(dataSet, treeFileOne, treeFileTwo, workDir, binaries, runID, taxaList, replicatesForNull, thisFilter, doFactor);
+			analysis.go();
+	}
+
+}

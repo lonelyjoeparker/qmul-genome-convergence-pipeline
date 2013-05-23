d8fb9b0e13dd4628e81a0edf1de0825f94b979b3
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2013-05-23 17:05:50 +0000

    Executable class to calculate parallel substitutions (using parsimony) in a subset of taxa.

---
+++ trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/runners/ParallelSubstitutionDetector.java
@@ -0,0 +1,83 @@
+package uk.ac.qmul.sbcs.evolution.convergence.runners;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import uk.ac.qmul.sbcs.evolution.convergence.ParsimonyReconstruction;
+import uk.ac.qmul.sbcs.evolution.convergence.TreeNode;
+import uk.ac.qmul.sbcs.evolution.convergence.util.SitewiseSpecificLikelihoodSupportAaml;
+import uk.ac.qmul.sbcs.evolution.convergence.util.serFilter;
+
+public class ParallelSubstitutionDetector {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ParallelSubstitutionDetector p = new ParallelSubstitutionDetector();
+		p.go(args);
+	}
+
+	private void go(String[] args){
+		System.out.println("input\tInputFileName\tmodel\tParallel H1\tParallel H1c\tParallel H2\tParallel H2c\t(Ambiguous at root)");
+		File dir = new File(args[0]);
+		FilenameFilter serFileFilter = new serFilter();
+		StringBuffer bufMain = new StringBuffer();				// no treefile buffer for this one.
+//		bufMain.append("locus\tensembl_URL\tshortcode\tdescription\thomog\tmodel\tmissingData\tlnl (species tree)\tlength (species tree)\tmodel alpha\tnumberOfTaxa\tnumberOfSites\tpreferredTopology");
+//		bufMain.append("\n");
+
+		Pattern specificPattern = Pattern.compile(args[1]);
+
+		if(dir.isDirectory()){
+			String[] serFilesList = dir.list(serFileFilter);
+			for(String someFile:serFilesList){
+				Matcher simMatch = specificPattern.matcher(someFile);
+				if(simMatch.find()){
+					InputStream serfile;
+					ObjectInputStream inOne;
+					SitewiseSpecificLikelihoodSupportAaml candidate;
+					try {
+						File input = new File(dir.getAbsolutePath()+"/"+someFile);
+						serfile = new FileInputStream(input);
+						inOne = new ObjectInputStream(serfile);
+						candidate = (SitewiseSpecificLikelihoodSupportAaml) inOne.readObject();
+						TreeNode species = new TreeNode(candidate.getFittedTrees()[0].replaceAll("\\s", ""),1);
+						HashMap<String, HashSet<String>[]> states = candidate.getDataset().getAminoAcidsAsFitchStates();
+						HashSet<String>[] baseStates = species.getFitchStates(states).clone();
+						int ambiguousAtRoot = 0;
+						for(HashSet<String> statesSet:baseStates){
+							if(statesSet.size()>1){
+								ambiguousAtRoot++;
+							}
+						}
+						species.resolveFitchStatesTopnode();
+						species.resolveFitchStates(species.states);
+						ParsimonyReconstruction pr = new ParsimonyReconstruction(states, species);
+						//pr.printAncestralComparison();
+						String[] echolocators = {"MEGADERMA","RHINOLOPHUS","MYOTIS","PTERONOTUS"};
+						int pll_H1 = pr.findParallelSubtitutionsFromAncestral(echolocators);
+						int pll_H1c= pr.findParallelSubtitutionsFromAncestralRejectingAmbiguities(echolocators,baseStates);
+						String[] echolocatorsH2 = {"TURSIOPS","MEGADERMA","RHINOLOPHUS","MYOTIS","PTERONOTUS"};
+						int pll_H2 = pr.findParallelSubtitutionsFromAncestral(echolocatorsH2);
+						int pll_H2c= pr.findParallelSubtitutionsFromAncestralRejectingAmbiguities(echolocatorsH2,baseStates);
+						System.out.println(input.getName()+"\t"+candidate.getInputFileName()+"\t"+candidate.getModel()+"\t"+pll_H1+"\t"+pll_H1c+"\t"+pll_H2+"\t"+pll_H2c+"\t"+ambiguousAtRoot);
+						species.getEndPos();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}

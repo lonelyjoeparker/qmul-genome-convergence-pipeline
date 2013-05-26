8895ed879922db5d75053eeb36c44d5e73e0d5b0
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2013-05-26 21:28:47 +0000

    Variation of CladeModelRegression to plot regression stats on divergent site class only.

---
+++ trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/runners/CladeModelRegressionDivergent.java
@@ -0,0 +1,83 @@
+package uk.ac.qmul.sbcs.evolution.convergence.runners;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import uk.ac.qmul.sbcs.evolution.convergence.handlers.documents.CladeModelOutput;
+import uk.ac.qmul.sbcs.evolution.convergence.util.SitewiseSpecificLikelihoodSupportAaml;
+import uk.ac.qmul.sbcs.evolution.convergence.util.stats.LinearRegression;
+
+public class CladeModelRegressionDivergent {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File serFile = new File(args[0]);
+		File rst = new File(args[1]);
+		boolean printSites = Boolean.parseBoolean(args[2]);
+		SitewiseSpecificLikelihoodSupportAaml candidate = null;
+		try{
+			InputStream serfile = new FileInputStream(serFile);
+			ObjectInputStream inOne = new ObjectInputStream(serfile);
+			candidate = (SitewiseSpecificLikelihoodSupportAaml) inOne.readObject();
+		}catch (Exception ex){
+			ex.printStackTrace();
+		}
+		CladeModelOutput cmo = new CladeModelOutput(rst);
+		HashMap<Integer,Double> siteOmegasAll		   	= cmo.getAllSiteOmegas();
+		HashMap<Integer,Double> siteOmegasDivergent = cmo.getDivergentSiteOmegas();
+		int numSites = candidate.getNumberOfSites();
+		int numTrees = candidate.getNumberOfTopologies();
+		float[][] SSLS = candidate.getSSLSseriesSitewise();
+		float dSSLS_sum = 0;
+		String parent = rst.getParent();
+		double[] x = new double[siteOmegasDivergent.size()];
+		double[] y = new double[siteOmegasDivergent.size()];
+		if(printSites){
+			for(int i=0;i<numSites;i++){
+				float dSSLS = SSLS[i][0] - SSLS[i][numTrees-1];
+				dSSLS_sum += dSSLS;
+				double w = siteOmegasAll.get(i);
+				System.out.println(parent+"\t"+i+"\t"+dSSLS+"\t"+w);
+//				x[i] = dSSLS;
+//				y[i] = w;
+			}
+		}else{
+			for(int i=0;i<numSites;i++){
+				float dSSLS = SSLS[i][0] - SSLS[i][numTrees-1];
+				dSSLS_sum += dSSLS;
+				double w = siteOmegasAll.get(i);
+//				x[i] = dSSLS;
+//				y[i] = w;
+			}
+		}
+		Iterator<Integer> siteItr = siteOmegasDivergent.keySet().iterator();
+		int index=0;
+		while(siteItr.hasNext()){
+			int key = siteItr.next();
+			float dSSLS = SSLS[key][0] - SSLS[key][numTrees-1];
+			x[index] = dSSLS;
+			y[index] = siteOmegasDivergent.get(key);
+			index++;
+		}
+		if(!printSites){
+			double mean_dSSLS = (dSSLS_sum / (float)numSites);
+			LinearRegression linear = new LinearRegression(x,y);
+			System.out.print(candidate.getInputFileName()+"\t");
+			System.out.print(parent+"\t");
+			System.out.print(mean_dSSLS+"\t");
+			System.out.print(linear.getRsq()+"\t");
+			System.out.print(linear.getBeta0_lo()+"\t");
+			System.out.print(linear.getBeta0()+"\t");
+			System.out.print(linear.getBeta0_hi()+"\t");
+			System.out.print(linear.getBeta1_lo()+"\t");
+			System.out.print(linear.getBeta1()+"\t");
+			System.out.print(linear.getBeta1_hi()+"\n");
+		}
+	}
+}

65993231a309aa4b7b025bf7d2ac03549f44bc49
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2014-10-23 13:19:05 +0000

    equals() and hashCode() implemented and tested for internal Branch and BranchPairComparison classes. getProbabilitiesForNodeComparisons() now implemented and tested. NullPointerException extended in NullBranchPairComparisonPointerException, to throw when a branch pair comparison not present in the data is queried.

--- trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/tests/CodemlAncestralSiteOutputParserTest.java
+++ trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/tests/CodemlAncestralSiteOutputParserTest.java
@@ -3,6 +3,7 @@ package uk.ac.qmul.sbcs.evolution.convergence.tests;
 import junit.framework.TestCase;
 import java.io.File;
 import uk.ac.qmul.sbcs.evolution.convergence.handlers.documents.parsers.codeml.CodemlAncestralSiteOutputParser;
+import uk.ac.qmul.sbcs.evolution.convergence.handlers.documents.parsers.codeml.CodemlAncestralSiteOutputParser.NullBranchPairComparisonPointerException;
 
 public class CodemlAncestralSiteOutputParserTest extends TestCase {
 
@@ -64,6 +65,7 @@ public class CodemlAncestralSiteOutputParserTest extends TestCase {
 		 */
 		String convergentSitesFilePath = System.getProperty("debugConvergentSitesFilePath");
 		CodemlAncestralSiteOutputParser parser = new CodemlAncestralSiteOutputParser(new File(convergentSitesFilePath));
+		System.out.println("constructor passed.");
 		boolean test_should_pass_node_A = !parser.containsNode(39); // should eval to 0
 		boolean test_should_pass_node_B = !parser.containsNode(12); // should eval to 0
 		boolean test_should_pass_node_C = !parser.containsNode(003); // should eval to 0, NB leading zeroes don't affect comparison
@@ -71,6 +73,8 @@ public class CodemlAncestralSiteOutputParserTest extends TestCase {
 		boolean test_should_fail_node_B = parser.containsNode(00); // should eval to 1
 		if(test_should_pass_node_A||test_should_pass_node_B||test_should_pass_node_C||test_should_fail_node_A||test_should_fail_node_B){
 			fail("hasNode tests failed: "+test_should_pass_node_A+""+test_should_pass_node_B+""+test_should_pass_node_C+""+test_should_fail_node_A+""+test_should_fail_node_B);
+		}else{
+			System.out.println("hasNode tests passed.");
 		}
 	}
 
@@ -105,9 +109,91 @@ public class CodemlAncestralSiteOutputParserTest extends TestCase {
 		 */
 		String convergentSitesFilePath = System.getProperty("debugConvergentSitesFilePath");
 		CodemlAncestralSiteOutputParser parser = new CodemlAncestralSiteOutputParser(new File(convergentSitesFilePath));
+		// should exist
 		int[] branch_A = {39,11};
 		int[] branch_B = {33,12};
-		float[] dataForThisPair = parser.getProbabilitiesForNodeComparisons(branch_A[0], branch_A[1], branch_B[0], branch_B[1]);
-		System.out.println(dataForThisPair[0]);
+		float[] dataForThisPair = null;
+		try {
+			dataForThisPair = parser.getProbabilitiesForNodeComparisons(branch_A[0], branch_A[1], branch_B[0], branch_B[1]);
+			System.out.println(dataForThisPair[0]);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(dataForThisPair == null){
+			fail("no branch pair comparison probabilities found for this pair");
+		}else{
+			System.out.println("Passed: branch pair comparison probabilities found for this pair: {"+ dataForThisPair[0] + ","+ dataForThisPair[1] + ","+ dataForThisPair[2] + ","+ dataForThisPair[3] + "}");
+		}
+		// now try with branches that should fail
+		dataForThisPair = null;
+		int[] branch_A_fail = {39,12};
+		int[] branch_B_fail = {33,11};
+		try {
+			dataForThisPair = parser.getProbabilitiesForNodeComparisons(branch_A_fail[0], branch_A_fail[1], branch_B_fail[0], branch_B_fail[1]);
+			System.out.println(dataForThisPair[0]);
+		} catch (NullBranchPairComparisonPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(dataForThisPair != null){
+			fail("branch pair comparison probabilities found for this pair, should not exist");
+		}else{
+			System.out.println("Passed: no branch pair comparison probabilities found for this pair, which should not exist.");
+		}
 	}
+	
+	public final void testSortIntPair(){
+		// make some calls to sortIntPair..
+		// should {0,0}
+		int[] return_0_0 = new CodemlAncestralSiteOutputParser().sortIntPair(0, 0);
+		// should {0,1}
+		int[] return_1_0 = new CodemlAncestralSiteOutputParser().sortIntPair(1, 0);
+		// should {0,1}
+		int[] return_0_1 = new CodemlAncestralSiteOutputParser().sortIntPair(0, 1);
+		// should {1,1}
+		int[] return_1_1 = new CodemlAncestralSiteOutputParser().sortIntPair(1, 1);
+		// should {-1,-1}
+		int[] return_m1_m1 = new CodemlAncestralSiteOutputParser().sortIntPair(-1, -1);
+		// should {-1,0}
+		int[] return_0_m1 = new CodemlAncestralSiteOutputParser().sortIntPair(0, -1);
+		// should {-1,1}
+		int[] return_1_m1 = new CodemlAncestralSiteOutputParser().sortIntPair(1, -1);
+		// should {max,max}
+		int[] return_max_max = new CodemlAncestralSiteOutputParser().sortIntPair(Integer.MAX_VALUE, Integer.MAX_VALUE);
+		// should {min,min}
+		int[] return_min_min = new CodemlAncestralSiteOutputParser().sortIntPair(Integer.MIN_VALUE, Integer.MIN_VALUE);
+		// should {min,max}
+		int[] return_max_min = new CodemlAncestralSiteOutputParser().sortIntPair(Integer.MAX_VALUE, Integer.MIN_VALUE);
+		// should {min,max}
+		int[] return_min_max = new CodemlAncestralSiteOutputParser().sortIntPair(Integer.MIN_VALUE, Integer.MAX_VALUE);
+		// should {min,max}
+		int[] return_0_min = new CodemlAncestralSiteOutputParser().sortIntPair(0, Integer.MIN_VALUE);
+		// should {0,max}
+		int[] return_max_0 = new CodemlAncestralSiteOutputParser().sortIntPair(Integer.MAX_VALUE, 0);
+		
+		/*
+		 * tests on outputs
+		 */
+		// simple positive ints
+		if(!((return_0_0[0]==0)&&(return_0_0[1]==0))){fail("0 0");}
+		if(!((return_1_0[0]==0)&&(return_1_0[1]==1))){fail("0 1");}
+		if(!((return_0_1[0]==0)&&(return_0_1[1]==1))){fail("0 1");}
+		if(!((return_1_1[0]==1)&&(return_1_1[1]==1))){fail("1 1");}
+		// signed ints
+		if(!((return_m1_m1[0]==-1)&&(return_m1_m1[1]==-1))){fail("-1 -1");}
+		if(!((return_0_m1[0]==-1)&&(return_0_m1[1]==0))){fail("-1 0");}
+		if(!((return_1_m1[0]==-1)&&(return_1_m1[1]==1))){fail("-1 1");}
+		// Integer.max and min vals
+		if(!((return_max_max[0]==Integer.MAX_VALUE)&&(return_max_max[1]==Integer.MAX_VALUE))){fail("max max");}
+		if(!((return_min_min[0]==Integer.MIN_VALUE)&&(return_min_min[1]==Integer.MIN_VALUE))){fail("min min");}
+		if(!((return_max_min[0]==Integer.MIN_VALUE)&&(return_max_min[1]==Integer.MAX_VALUE))){fail("min max");}
+		if(!((return_min_max[0]==Integer.MIN_VALUE)&&(return_min_max[1]==Integer.MAX_VALUE))){fail("min max");}
+		if(!((return_0_min[0]==Integer.MIN_VALUE)&&(return_0_min[1]==0))){fail("min 0");}
+		if(!((return_max_0[0]==0)&&(return_max_0[1]==Integer.MAX_VALUE))){fail("0 max");}
+		
+		// test complete if it reached here
+		System.out.println("Test sortIntPair() complete.");
+	}
+
 }

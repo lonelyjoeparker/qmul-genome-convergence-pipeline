ef68b9eded4261b8df773df57f2fb42c1593f21d
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2014-12-10 13:24:08 +0000

    Model-View-Controller for phylogenies. Basic functions implements.

---
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/gui/models/PhylogeniesModel.java
@@ -0,0 +1,88 @@
+package uk.ac.qmul.sbcs.evolution.convergence.gui.models;
+
+import java.io.File;
+
+import javax.swing.table.AbstractTableModel;
+
+import uk.ac.qmul.sbcs.evolution.convergence.gui.DisplayPhylogeny;
+
+public class PhylogeniesModel extends AbstractTableModel{
+
+	/* Object variables */
+	
+	public final String[] columnNames = new String[]{"File","Number of phylogenies","First phylogeny"};
+	private Object[][] data;
+	
+	/* Utility methods for TableModel type behaviour */
+	
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return data.length;
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		return data[row][col];
+	}
+
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+
+	/*
+	 * JTable uses this method to determine the default renderer/
+	 * editor for each cell.  If we didn't implement this method,
+	 * then the last column would contain text ("true"/"false"),
+	 * rather than a check box.
+	 */
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	public Object[][] getData(){
+		return data;
+	}
+
+	/* Constructors */
+	
+	/**
+	 * Default no-arg constructor
+	 */
+	public PhylogeniesModel(){
+		// instantiate the table, although this is a bit risky as null vals in first row...
+		data = new Object[1][3];
+	}
+	
+	/* Object methods for specific PhylogenyModel-y things... */
+	
+	/**
+	 * Add a new phylogeny to the TableModel as a File.
+	 */
+	public void addPhylogenyRowAsStringTree(File newTreeAsFile){
+		Object[][] newData = new Object[data.length+1][data[0].length];
+		for(int i=0;i<data.length;i++){
+			newData[i] = data[i];
+		}
+		Object[] newRow = new Object[this.getColumnCount()];
+		DisplayPhylogeny dp = new DisplayPhylogeny(newTreeAsFile);
+		newRow[0] = dp;
+		try {
+			newRow[1] = dp.getNewickTree().getNumberOfTrees();
+		} catch (Exception e) {
+			newRow[1] = 0;
+		}
+		try {
+			newRow[2] = dp.getTextTreeRepresentation();
+		} catch (Exception e) {
+			newRow[2] = "();";
+		}
+		newData[data.length] = newRow;
+		data = newData;
+		this.fireTableRowsInserted(data.length-1, data.length-1);
+	}
+}

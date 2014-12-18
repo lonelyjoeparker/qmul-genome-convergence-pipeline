1df19c5d653c4ef4f9def34c3cb1438f89483711
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2014-12-18 16:22:01 +0000

    Basic model-view-controller for Analyses in GUI.

---
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/gui/models/AnalysesModel.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package uk.ac.qmul.sbcs.evolution.convergence.gui.models;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * @author <a href="mailto:joe@kitson-consulting.co.uk">Joe Parker, Kitson Consulting / Queen Mary University of London</a>
+ *
+ */
+public class AnalysesModel extends AbstractTableModel {
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableModel#getColumnCount()
+	 */
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableModel#getRowCount()
+	 */
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.table.TableModel#getValueAt(int, int)
+	 */
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ trunk/src/uk/ac/qmul/sbcs/evolution/convergence/gui/views/AnalysesView.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package uk.ac.qmul.sbcs.evolution.convergence.gui.views;
+
+import javax.swing.JComponent;
+
+/**
+ * @author <a href="mailto:joe@kitson-consulting.co.uk">Joe Parker, Kitson Consulting / Queen Mary University of London</a>
+ *
+ */
+public class AnalysesView extends JComponent {
+
+}

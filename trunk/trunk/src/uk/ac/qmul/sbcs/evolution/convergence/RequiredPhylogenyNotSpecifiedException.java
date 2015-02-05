2ecfbe230ff4a394c63fb78b0040892703b0ea7f
joeparkerandthemegahairymen@gmail.com <joeparkerandthemegahairymen@gmail.com@cbd360a1-42f0-d037-de4f-95f32daef414>
2015-02-05 18:25:28 +0000

    Subclassed java.lang.Exception to be thrown when required number/PhylogenyConvergenceContext of phylogenies present in dataset at analysis set-up.

---
+++ trunk/trunk/src/uk/ac/qmul/sbcs/evolution/convergence/RequiredPhylogenyNotSpecifiedException.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package uk.ac.qmul.sbcs.evolution.convergence;
+
+/**
+ * @author <a href="mailto:joe@kitson-consulting.co.uk">Joe Parker, Kitson Consulting / Queen Mary University of London</a>
+ *
+ */
+public class RequiredPhylogenyNotSpecifiedException extends Exception {
+
+	/**
+	 * 
+	 */
+	public RequiredPhylogenyNotSpecifiedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public RequiredPhylogenyNotSpecifiedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public RequiredPhylogenyNotSpecifiedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public RequiredPhylogenyNotSpecifiedException(String message,
+			Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
